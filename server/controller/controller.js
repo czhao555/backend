ce38bccd6b7596d34d4ffd41aa4bd831f92da8f1
Zhen hao <zhenhaochoo39.com>
2020-12-08 13:56:43 +0800

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -18,10 +18,10 @@ exports.create = (req, res) => {
 
   // save user in the database
   user
-    .save(user)
+    .save()
     .then((data) => {
-      //res.send(data)
-      res.redirect("/add-user");
+      res.send(data);
+      //res.redirect("/add-user");
     })
     .catch((err) => {
       res.status(500).send({
